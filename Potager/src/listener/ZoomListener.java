1ace26b6628e2f67291bd1156bc929e623aacd0c
tanguy <tanguy.fil@gmail.com>
2015-01-26 22:59:36 +0100

    Ajout des listeners de la gui

---
+++ Potager/src/listener/ZoomListener.java
@@ -0,0 +1,32 @@
+package listener;
+
+import gui.JTerrainMap;
+import gui.ToolsPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ZoomListener implements ActionListener {
+
+	private JTerrainMap jt;
+	private ToolsPanel tools;
+
+	public ZoomListener(JTerrainMap jt, ToolsPanel tools) {
+		this.jt = jt;
+		this.tools = tools;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Object source = arg0.getSource();
+		if (source==this.tools.getZoomIn()){
+			System.out.println("zoom plus");
+			this.jt.taillecaseplus();
+		}
+		else if (source==this.tools.getZoomOut()){
+			this.jt.taillecasemoins();
+		}
+
+	}
+
+}
